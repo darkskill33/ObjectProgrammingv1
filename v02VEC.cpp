b77be36b4bf245f40fb5e6dd1e28becd5d0dde9c
darkskill33 <116464783+darkskill33@users.noreply.github.com>
2023-03-01 12:40:02 +0200

    Add files via upload

---
+++ v02VEC.cpp
@@ -0,0 +1,269 @@
+#include "Mylib.h"
+
+
+ofstream fr ("rezultatai.txt");
+
+struct studentas //struct
+{
+    string vardas, pavarde;
+    vector<int> paz;
+    double egz;
+    double gal;
+    double med;
+};
+
+bool isnumber(string str) //check if string is number;
+{
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(!isdigit(str[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void sort(studentas &temp, int n) //number sorting function
+{
+    for (int i = 0; i < n; i++)
+    {  
+        for (int j = i + 1 ; j < n; j++)
+        {
+            if(temp.paz[i] < temp.paz[j])
+            {
+                swap(temp.paz[i], temp.paz[j]);
+            }
+        }
+    }
+}
+
+void sortname(vector<studentas> &mas) //name sorting function
+{
+    int a = mas.size();
+    for (int i = 0; i < a; i++)
+    {  
+        for (int j = i + 1 ; j < a-1; j++)
+        {
+            if(mas[i].vardas > mas[j].vardas)
+            {
+                swap(mas[i], mas[j]);
+            } else if(mas[i].vardas == mas[j].vardas) //if names are the same, we sort by last name;
+                    {
+                        if(mas[i].pavarde < mas[j].pavarde)
+                        {   
+                            swap(mas[i], mas[j]);
+                        }
+                    }
+        }
+    }
+}
+
+void mediana(studentas &temp, int n) //mediana search function;
+{
+    sort(temp, n);
+    float mid1, mid2;
+    if(n == 2*(n/2))
+    {
+        mid1 = temp.paz[n/2 - 1];
+        mid2 = temp.paz[n/2];
+        temp.med = (mid1+mid2)/2;
+    } 
+    else 
+    {
+        mid1 = temp.paz[n/2];
+        temp.med = mid1;
+    }
+}
+
+void pild(studentas& temp) //function to insert data by hand;
+{
+    cout << "Iveskite varda ir pavarde: "; cin >> temp.vardas >> temp.pavarde;
+    char pas;
+    cout << "Jei norite atsitiktinai sugeneruoti pažymius ir egzamino rezultatą, įveskite (k), jei duomenis įvesite pats, įveskite (s): " << endl;
+    do
+    {
+        cin >> pas;
+    } while (pas != 's' && pas != 'S' && pas != 'k' && pas != 'K' );
+
+    string check;
+    if (pas == 's' || pas == 'S')
+    {
+        cout << "Iveskite pazymius (norint nebevesti pažymiu rašykite: '0' ): ";
+        while(true)
+        {
+            cin >> check;
+            if(!isnumber(check)) 
+            {
+                cout << "Iveskite SKAICIU [1; 10] be simboliu ar raidziu. Į intervalą nepatenkantys skaičiai nebus apdorojami" << endl;
+                continue;
+            }
+
+            int check_nd = stoi(check);
+        
+            if(check_nd == 0)
+            {
+                if(temp.paz.size() == 0)
+                {
+                    temp.paz.push_back(check_nd);
+                    break;
+                } else break;
+            } else if(check_nd > 0 && check_nd <=10)
+                    {
+                        temp.paz.push_back(check_nd);
+                    } else if(check_nd < 0 || check_nd > 10)
+                        {
+                            cout << "Iveskite SKAICIU intervale [1; 10]: " << endl;
+                            continue;
+                        }
+        }
+
+        cout << "Iveskite egzamino paz.: ";
+        while(true)
+        {
+            cin >> check;
+            if(!isnumber(check)) 
+            {
+                cout << "Iveskite SKAICIU [1; 10] be simboliu ar raidziu. Į intervalą nepatenkantys skaičiai nebus abdorojami" << endl;
+                continue;
+            }
+
+            int check_nd = stoi(check);
+            if(check_nd > 0 && check_nd <=10)
+                {
+                    temp.egz = check_nd;
+                } else if(check_nd < 0 || check_nd > 10)
+                        {
+                            cout << "Iveskite SKAICIU intervale: [1; 10]" << endl;
+                            continue;
+                        }
+            break;
+        }
+
+    } else if(pas == 'k' || pas == 'K')
+        { 
+            cout << "Generuojami pažymiai: " << endl;
+            srand(time(0));
+            int a = (rand()%10)+1; //generating amount of random numbers
+            for (int i = 0; i < a; i++)
+            {
+                int c = (rand()%10)+1; //generating random numbers;
+                cout << c << " ";
+                temp.paz.push_back(c);
+            }
+            cout << endl;
+            cout << "Generuojamas egzamino pažymys: " << endl;
+            int c = (rand()%10)+1; //generating random exam grade;
+            cout << c << endl;
+            temp.egz = c;
+        } 
+}
+
+void eil_po_eil(string read_vardas, studentas& temp, vector<studentas> &mas) //read data from file;
+{
+    string eil, zod, skaic;
+    int times;
+    ifstream open_f(read_vardas);
+    getline(open_f, eil);
+    times = count(eil.begin(), eil.end(), 'N'); //counting a number of homework;
+    while (open_f)
+    { 
+      if (!open_f.eof()) 
+        {
+            open_f >> temp.vardas >> temp.pavarde;
+            for(int i = 0; i < times; i++)
+            {
+                int a;
+                open_f >> a;
+                temp.paz.push_back(a);
+            }
+            int a;
+            open_f >> a; 
+            temp.egz = a;
+            mas.push_back(temp);
+            temp.paz.resize(0);
+        }   else break;
+    }
+}
+
+void spausd(studentas& temp, char ats) //printing data to file;
+{
+    fr  << left << setw(16) << temp.vardas  << left << setw(14) << temp.pavarde;
+    for(int i = 0; i < temp.paz.size(); i++)
+    {
+        temp.gal += temp.paz[i];
+    } 
+    
+    if (ats == 'G' || ats == 'g')
+    {
+        double kint = (0.4*temp.gal/(double)temp.paz.size() +  temp.egz*0.6);
+        fr << left << setw(19)  << setprecision(2) << fixed << kint  << " - " << endl;  
+    }
+
+    if(ats == 'M' || ats == 'm')
+    {
+        mediana(temp, temp.paz.size());
+        fr << left << setw(19)  << " - " << (0.4*temp.med + temp.egz*0.6)<< endl;
+    }
+}
+
+int main()
+{   
+    vector<studentas> mas;
+    studentas tempas;
+
+    char rink;
+    cout << "Jei norite:" << endl;
+    do
+    {
+        cout << "nuskaityti duomenis iš failo, įveskite (f). " << endl;
+        cout << "įvesti duomenis ranka, įveskite (r). " << endl;
+        cin >> rink; 
+    } while (rink != 'r' && rink != 'R' && rink != 'f' && rink != 'F');
+    
+    
+
+    if(rink == 'r' || rink == 'R')
+    {
+        char uzkl = 'n';
+        do{
+            pild(tempas);
+            mas.push_back(tempas);
+            tempas.paz.clear();
+            cout << "------------------------------------------------------------------------------" <<endl;
+            cout << "Baigti darba spausk n, testi - bet koks klavisas: " << endl;
+            cout << "------------------------------------------------------------------------------" <<endl;
+            cin >> uzkl;
+        }while (uzkl!='n' && uzkl!='N');   
+    } else if(rink == 'f' || rink == 'F')
+            {
+                eil_po_eil("kursiokai.txt", tempas, mas);
+            }
+    
+
+    
+    char ats;  
+    do
+    {
+        cout << "Galutinį pažymį(Vidurkis), rašykite (G)" << endl;
+        cout << "Galutinį pažymį(Mediana), rašykite (M)" << endl;
+        cin >> ats;
+        cout << endl;
+    } while (ats != 'g' && ats != 'G' && ats != 'm' && ats != 'M');
+
+    sortname(mas); //data sorting by name/lastname;
+    
+    fr << "------------------------------------------------------------------------------" <<endl;
+    fr  << left << setw(16) << "Vardas"  << left << setw(14) << "Pavardė " << left << setw(12)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
+    fr << "------------------------------------------------------------------------------" <<endl;
+
+    for(auto &i : mas) spausd(i, ats);
+    for(auto &i : mas) i.paz.clear();
+    mas.clear();
+
+    cout << "Pasitikrinkite savo rezultatų failą!" << endl;
+}
+
+
+
+
