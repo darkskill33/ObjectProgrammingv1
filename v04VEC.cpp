1534bb6801ce8631033bd5136149e21ff9576d51
darkskill33 <116464783+darkskill33@users.noreply.github.com>
2023-03-13 15:56:45 +0200

    read more
    - Generuojami mokinių duomenų failai; 
    - Mokiniai skaidomi į dvi grupes;
    - Skaičiuojami procesų laikai;
    - Mokinių rūšiavimas pagal pažymius.

--- v04VEC.cpp
+++ v04VEC.cpp
@@ -9,6 +9,7 @@ struct studentas //struct
     double egz;
     double gal;
     double med;
+    double lygin;
 };
 
 bool isnumber(string str) //check if string is number;
@@ -34,10 +35,7 @@ bool equality(studentas &x, studentas &y)
 
 bool grading(studentas &x, studentas &y)
 {
-  if(x.pavarde == y.pavarde) 
-    {
-        return x.pavarde < y.pavarde;
-    } else return x.vardas < y.vardas;  
+    return x.lygin > y.lygin;
 }
 
 void mediana(studentas &temp, int n) //mediana search function;
@@ -140,7 +138,7 @@ void pild(studentas& temp) //function to insert data by hand;
         } 
 }
 
-void eil_po_eil(string read_vardas, studentas& temp, vector<studentas> &mas) //read data from file;
+void eil_po_eil(string read_vardas, studentas& temp, vector<studentas> &mas, double laikas) //read data from file;
 {
     string eil, zod, skaic;
     int times;
@@ -182,6 +180,7 @@ void eil_po_eil(string read_vardas, studentas& temp, vector<studentas> &mas) //r
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end-start; // Skirtumas (s)
     cout << "Failo nuskaitymas užtruko: "<< diff.count() << " s." << endl;   
+    laikas += diff.count();
 }
 /*
 void spausd(vector<studentas> &mas, char ats) //printing data to file;
@@ -213,60 +212,61 @@ void spausd(vector<studentas> &mas, char ats) //printing data to file;
     }
 }
 */
-void spausd(vector<studentas> &mas, char ats) //printing data to file;
-{
-    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
-    ofstream fs ("stropus.txt");
-    ofstream fr ("apsileide.txt");
-    fr << "------------------------------------------------------------------------------" <<endl;
-    fr  << left << setw(16) << "Vardas"  << left << setw(25) << "Pavardė " << left << setw(20)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
-    fr << "------------------------------------------------------------------------------" <<endl;
+// void spausd(vector<studentas> &mas, char ats) //printing data to file;
+// {
+//     auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+//     ofstream fs ("stropus.txt");
+//     ofstream fr ("apsileide.txt");
+//     fr << "------------------------------------------------------------------------------" <<endl;
+//     fr  << left << setw(16) << "Vardas"  << left << setw(25) << "Pavardė " << left << setw(20)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
+//     fr << "------------------------------------------------------------------------------" <<endl;
 
-    fs << "------------------------------------------------------------------------------" <<endl;
-    fs  << left << setw(16) << "Vardas"  << left << setw(25) << "Pavardė " << left << setw(20)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
-    fs << "------------------------------------------------------------------------------" <<endl;
+//     fs << "------------------------------------------------------------------------------" <<endl;
+//     fs  << left << setw(16) << "Vardas"  << left << setw(25) << "Pavardė " << left << setw(20)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
+//     fs << "------------------------------------------------------------------------------" <<endl;
 
-    for(auto &k : mas)
-    {
-        //fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
-        for(int i = 0; i < k.paz.size(); i++)
-        {
-            k.gal += k.paz[i];
-        } 
+//     for(auto &k : mas)
+//     {
+//         //fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+//         for(int i = 0; i < k.paz.size(); i++)
+//         {
+//             k.gal += k.paz[i];
+//         } 
     
-        if (ats == 'G' || ats == 'g')
-        {
-            double kint = (0.4*k.gal/(double)k.paz.size() +  k.egz*0.6);
-            if(kint < 5)
-            {
-                fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
-                fr << left << setw(20)  << setprecision(2) << fixed << kint  << " - " << endl;  
-            } else 
-                {
-                    fs  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
-                    fs << left << setw(20)  << setprecision(2) << fixed << kint  << " - " << endl;
-                }
-        }
+//         if (ats == 'G' || ats == 'g')
+//         {
+//             double kint = (0.4*k.gal/(double)k.paz.size() +  k.egz*0.6);
+//             if(kint < 5)
+//             {
+//                 fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+//                 fr << left << setw(20)  << setprecision(2) << fixed << kint  << " - " << endl;  
+//             } else 
+//                 {
+//                     fs  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+//                     fs << left << setw(20)  << setprecision(2) << fixed << kint  << " - " << endl;
+//                 }
+//         }
 
-        if(ats == 'M' || ats == 'm')
-        {
-            mediana(k, k.paz.size());
-            double kint = (0.4*k.med + k.egz*0.6);
-            if(kint < 5)
-            {
-                fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
-                fr << left << setw(20)   << " - "  << fixed << setprecision(2) << kint << endl;  
-            } else 
-                {
-                    fs  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
-                    fs << left << setw(20) << " - "  << fixed << setprecision(2) << kint  << endl;
-                }
-        }
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start; // Skirtumas (s)
-    cout << "Failo rūšiavimas į dvi grupes ir išvedimas į du failus užtruko: "<< diff.count() << " s." << endl;  
-}
+//         if(ats == 'M' || ats == 'm')
+//         {
+//             mediana(k, k.paz.size());
+//             double kint = (0.4*k.med + k.egz*0.6);
+//             if(kint < 5)
+//             {
+//                 fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+//                 fr << left << setw(20)   << " - "  << fixed << setprecision(2) << kint << endl;  
+//             } else 
+//                 {
+//                     fs  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+//                     fs << left << setw(20) << " - "  << fixed << setprecision(2) << kint  << endl;
+//                 }
+//         }
+//     }
+//     auto end = std::chrono::high_resolution_clock::now();
+//     std::chrono::duration<double> diff = end-start; // Skirtumas (s)
+//     cout << "Failo rūšiavimas į dvi grupes ir išvedimas į du failus užtruko: "<< diff.count() << " s." << endl;  
+// }
+
 
 void generate(string pavadinimas, int size)
 {
@@ -275,7 +275,7 @@ void generate(string pavadinimas, int size)
     srand(time(0));
     int pazym_sk = (rand()%20)+1; //generating random homework grades number. NOW ITS [1; 20];
     //(start) Creating top line (vardas pavarde ND* Egz.);
-    fs  << left << setw(16) << "Vardas"  << left << setw(14) << "Pavarde" << left << setw(20);
+    fs  << left << setw(16) << "Vardas"  << left << setw(14) << "Pavarde" << left << setw(20) << " ";
     for(int i = 1; i <= pazym_sk; i++)
     {
         fs << left << setw(10) << ("ND" + std::to_string(i));
@@ -299,11 +299,98 @@ void generate(string pavadinimas, int size)
     cout << "Failo generavimas užtruko: "<< diff.count() << " s." << endl; 
 }
 
+void skirstymas(vector<studentas> &mas, char ats, vector<studentas> &mldc, vector<studentas> &blgj, double &laikas) //sorting students to good and bad ones;
+{
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+    for(auto &k : mas)
+    {
+        for(int i = 0; i < k.paz.size(); i++)
+        {
+            k.gal += k.paz[i];
+        } 
+
+        if (ats == 'G' || ats == 'g')
+        {
+            double kint = (0.4*k.gal/(double)k.paz.size() +  k.egz*0.6);
+            k.lygin = kint;
+            //k.paz.erase(k.paz.begin(), k.paz.end());
+            if(kint < 5)
+            {
+                blgj.push_back(k);
+            } else 
+                {
+                    mldc.push_back(k);
+                }
+    
+        }
+
+        if(ats == 'M' || ats == 'm')
+        {
+            mediana(k, k.paz.size());
+            double kint = (0.4*k.med + k.egz*0.6);
+            k.lygin = kint;
+            //k.paz.erase(k.paz.begin(), k.paz.end());
+            if(kint < 5)
+            {
+                blgj.push_back(k);
+            } else 
+                {
+                    mldc.push_back(k);
+                }
+        }        
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start; // Skirtumas (s)
+    cout << "Failo suskirstymas į vargšus ir šaunuolius užtruko: "<< diff.count() << " s." << endl;
+    laikas += diff.count();
+
+    start = std::chrono::high_resolution_clock::now();
+    sort(mldc.begin(), mldc.end(), grading); //data sorting by grades;    
+    sort(blgj.begin(), blgj.end(), grading); //data sorting by grades;   
+
+    end = std::chrono::high_resolution_clock::now();
+    diff = end-start; // Skirtumas (s)
+    cout << "Grupių rūšiavimas vargšai ir šaunuoliai užtruko: "<< diff.count() << " s." << endl;
+    laikas += diff.count();
+
+}
+
+void spausd(vector<studentas> &mas, char ats, string file_name, double &laikas) //printing data to file;
+{
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+    ofstream fr (file_name);
+    fr << "------------------------------------------------------------------------------" <<endl;
+    fr << left << setw(16) << "Vardas"  << left << setw(25) << "Pavardė " << left << setw(20)  << "Galutinis(Vid.)/Galutinis(Med.)"<<  endl;
+    fr << "------------------------------------------------------------------------------" <<endl;
+
+    for(auto &k : mas)
+    {
+        if (ats == 'G' || ats == 'g')
+        {
+            fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+            fr << left << setw(20)  << setprecision(2) << fixed << k.lygin  << " - " << endl;
+        }
+
+        if(ats == 'M' || ats == 'm')
+        {
+            fr  << left << setw(16) << k.vardas  << left << setw(25) << k.pavarde;
+            fr << left << setw(20)   << " - "  << fixed << setprecision(2) << k.lygin << endl;
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start; // Skirtumas (s)
+    cout << "Grupės " << file_name << " išvedimas į failą užtruko: "<< diff.count() << " s." << endl;  
+    laikas += diff.count();
+}
+
 int main()
 {   
     vector<studentas> mas;
     studentas tempas;
-
+    vector<studentas> mldc;
+    vector<studentas> blgj;
+    double laikas = 0;
     char rink;
     cout << "Jei norite:" << endl;
     do
@@ -330,7 +417,7 @@ int main()
         }while (uzkl!='n' && uzkl!='N');  
     } else if(rink == 'f' || rink == 'F')
             {
-                eil_po_eil("sarasas10000000.txt", tempas, mas);
+                eil_po_eil("sarasas10000000.txt", tempas, mas, laikas);
             } else if (rink == 's' || rink == 'S')
                 {
                     int size;
@@ -352,10 +439,16 @@ int main()
     } while (ats != 'g' && ats != 'G' && ats != 'm' && ats != 'M');
 
     sort(mas.begin(), mas.end(), equality); //data sorting by name/lastname;  If "equality" function returns "False", it let's us know, that the "First" argument shouldn't be placed before "Second" argument, arguments would be swaped;
-
-    spausd(mas, ats); //printing data to file;
+    skirstymas(mas, ats, mldc, blgj, laikas);
+    spausd(mldc, ats, "stiprus.txt", laikas); //printing data to file;
+    spausd(blgj, ats, "apsileide.txt", laikas); //printing data to file;
+    cout << "Programos trukmės laikas: " << laikas << "s. \n";
     for(auto &i : mas) i.paz.clear();
     mas.clear();
+    for(auto &i : mldc) i.paz.clear();
+    mldc.clear();
+    for(auto &i : blgj) i.paz.clear();
+    blgj.clear();
 
     cout << "Pasitikrinkite savo rezultatų failą!" << endl;
 }
